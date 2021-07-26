083ec988ba00e4ac746eea6cb10b15a58d4d22b2
Andrew Fergusson <andrew.fergusson@me.com>
2021-07-26 16:01:30 +0000

    fixed test_uritests.py

--- csvqb/csvqb/tests/unit/utils/test_uritests.py
+++ csvqb/csvqb/tests/unit/utils/test_uritests.py
@@ -5,14 +5,12 @@ from csvqb.tests.unit.test_baseunit import *
 from csvqb.utils.uri import get_last_uri_part, csvw_column_name_safe
 
 
-
 def test_uri_last_part():
-    pytest.assertEqual("dataset-name#something", get_last_uri_part("http://gss-data.org.uk/data/stuff/dataset-name#something"))
+    assert "dataset-name#something" == get_last_uri_part("http://gss-data.org.uk/data/stuff/dataset-name#something")
 
 def test_csvw_column_name():
-    pytest.assertEqual("some_random_column_name", csvw_column_name_safe("Some Random Column //+Name"))
-    pytest.assertEqual("something_else", csvw_column_name_safe("Something-else"))
-
+    assert "some_random_column_name" == csvw_column_name_safe("Some Random Column //+Name")
+    assert "something_else" == csvw_column_name_safe("Something-else")
 
 if __name__ == '__main__':
     pytest.main()
