758522be8fc0c3fbec47db97b490ea35e6bd1ffe
Rob Barry <rob.barry@ons.gov.uk>
2021-07-27 09:41:07 +0100

    Correcting comment.

--- csvqb/csvqb/models/rdf/qbdatasetincatalog.py
+++ csvqb/csvqb/models/rdf/qbdatasetincatalog.py
@@ -3,7 +3,7 @@ from sharedmodels.rdf import dcat, qb
 
 class QbDataSetInCatalog(qb.DataSet, dcat.Dataset):
     """
-    Represents both a skos:ConceptScheme and a dcat:Dataset in one node. Means that we don't have to link
+    Represents both a qb:DataSet and a dcat:Dataset in one node. Means that we don't have to link
     between the two.
     """
 
