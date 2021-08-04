87e04551e7b3bbd1ccc62782777df0fdcb631721
Rob Barry <rob.barry@ons.gov.uk>
2021-08-04 14:28:02 +0100

    Issue #75 - Implemented all prescribed tests.

--- sharedmodels/sharedmodels/tests/unit/rdf/test_resource_serialisation.py
+++ sharedmodels/sharedmodels/tests/unit/rdf/test_resource_serialisation.py
@@ -1,18 +1,39 @@
-import pytest
 from typing import Annotated
-from rdflib import URIRef, Graph, Literal
 
-from sharedmodels.rdf.resource import NewResource, map_str_to_en_literal, map_resource_to_uri, Resource, ExistingResource
-from sharedmodels.rdf.triple import Triple, PropertyStatus
+import pytest
+from rdflib import URIRef, Graph, Literal
+from sharedmodels.rdf.resource import (
+    NewResource,
+    map_str_to_en_literal,
+    map_resource_to_uri,
+    Resource,
+    ExistingResource,
+)
+from sharedmodels.rdf.triple import Triple, InverseTriple, PropertyStatus
+
 
 def test_overriding_attribute_annotations():
     """Test that when an annotated property is overridden in a sub-class, the overriding annotation wins out."""
 
     class A(NewResource):
-        p: Annotated[str, Triple(URIRef("http://original-uri"), PropertyStatus.mandatory, map_str_to_en_literal)]
+        p: Annotated[
+            str,
+            Triple(
+                URIRef("http://original-uri"),
+                PropertyStatus.mandatory,
+                map_str_to_en_literal,
+            ),
+        ]
 
     class B(A):
-        p: Annotated[str, Triple(URIRef("http://replacement-uri"), PropertyStatus.mandatory, map_str_to_en_literal)]
+        p: Annotated[
+            str,
+            Triple(
+                URIRef("http://replacement-uri"),
+                PropertyStatus.mandatory,
+                map_str_to_en_literal,
+            ),
+        ]
 
     b = B("http://some-b-uri")
     b.p = "Hello, World"
@@ -20,16 +41,40 @@ def test_overriding_attribute_annotations():
     graph = b.to_graph(Graph())
 
     assert 1 == len(graph)
-    assert (URIRef("http://some-b-uri"), URIRef("http://replacement-uri"), Literal("Hello, World", "en")) in graph
-    assert (URIRef("http://some-b-uri"), URIRef("http://original-uri"), Literal("Hello, World", "en")) not in graph
+    assert (
+        URIRef("http://some-b-uri"),
+        URIRef("http://replacement-uri"),
+        Literal("Hello, World", "en"),
+    ) in graph
+    assert (
+        URIRef("http://some-b-uri"),
+        URIRef("http://original-uri"),
+        Literal("Hello, World", "en"),
+    ) not in graph
+
 
 def test_resource_serialised_for_new_resource():
     """Ensure that we can use an `NewResource` when providing a `Resource` for serialisation."""
+
     class A(NewResource):
-        p_a: Annotated[str, Triple(URIRef("http://some-predicate-a"), PropertyStatus.optional, map_str_to_en_literal)]
+        p_a: Annotated[
+            str,
+            Triple(
+                URIRef("http://some-predicate-a"),
+                PropertyStatus.optional,
+                map_str_to_en_literal,
+            ),
+        ]
 
     class B(NewResource):
-        p_b: Annotated[Resource[A], Triple(URIRef("http://some-predicate-b"), PropertyStatus.mandatory, map_resource_to_uri)]
+        p_b: Annotated[
+            Resource[A],
+            Triple(
+                URIRef("http://some-predicate-b"),
+                PropertyStatus.mandatory,
+                map_resource_to_uri,
+            ),
+        ]
 
     a = A("http://some-new-resource-a")
     a.p_a = "Hello, World"
@@ -39,26 +84,53 @@ def test_resource_serialised_for_new_resource():
 
     graph = b.to_graph(Graph())
 
-    assert (URIRef("http://some-new-resource-b"), URIRef("http://some-predicate-b"), URIRef("http://some-new-resource-a")) in graph
+    assert (
+        URIRef("http://some-new-resource-b"),
+        URIRef("http://some-predicate-b"),
+        URIRef("http://some-new-resource-a"),
+    ) in graph
 
     # Ensure that we recurviely serialise new resources and their triples.
-    assert (URIRef("http://some-new-resource-a"), URIRef("http://some-predicate-a"), Literal("Hello, World", "en")) in graph
+    assert (
+        URIRef("http://some-new-resource-a"),
+        URIRef("http://some-predicate-a"),
+        Literal("Hello, World", "en"),
+    ) in graph
 
 
 def test_resource_serialised_for_existing_resource():
     """Ensure that we can use an `ExistingResource` inplace of a `NewResource` when providing a `Resource` for serialisation."""
+
     class A(NewResource):
-        p_a: Annotated[str, Triple(URIRef("http://some-predicate-a"), PropertyStatus.optional, map_str_to_en_literal)]
+        p_a: Annotated[
+            str,
+            Triple(
+                URIRef("http://some-predicate-a"),
+                PropertyStatus.optional,
+                map_str_to_en_literal,
+            ),
+        ]
 
     class B(NewResource):
-        p_b: Annotated[Resource[A], Triple(URIRef("http://some-predicate-b"), PropertyStatus.mandatory, map_resource_to_uri)]
+        p_b: Annotated[
+            Resource[A],
+            Triple(
+                URIRef("http://some-predicate-b"),
+                PropertyStatus.mandatory,
+                map_resource_to_uri,
+            ),
+        ]
 
     b = B("http://some-new-resource-b")
     b.p_b = ExistingResource("http://some-existing-resource-a")
 
     graph = b.to_graph(Graph())
 
-    assert (URIRef("http://some-new-resource-b"), URIRef("http://some-predicate-b"), URIRef("http://some-existing-resource-a")) in graph
+    assert (
+        URIRef("http://some-new-resource-b"),
+        URIRef("http://some-predicate-b"),
+        URIRef("http://some-existing-resource-a"),
+    ) in graph
 
 
 def test_resource_serialised_for_circular_reference():
@@ -74,19 +146,120 @@ def test_resource_serialised_for_circular_reference():
 
     graph = b.to_graph(Graph())
 
-    assert (URIRef("http://some-new-resource-b"), URIRef("http://some-predicate-b"), URIRef("http://some-new-resource-a")) in graph
-    assert (URIRef("http://some-new-resource-a"), URIRef("http://some-predicate-a"), URIRef("http://some-new-resource-b")) in graph
+    assert (
+        URIRef("http://some-new-resource-b"),
+        URIRef("http://some-predicate-b"),
+        URIRef("http://some-new-resource-a"),
+    ) in graph
+    assert (
+        URIRef("http://some-new-resource-a"),
+        URIRef("http://some-predicate-a"),
+        URIRef("http://some-new-resource-b"),
+    ) in graph
 
 
 class A_Circular(NewResource):
-    p_a: Annotated[Resource["B_Circular"], Triple(URIRef("http://some-predicate-a"), PropertyStatus.mandatory,
-                                                  map_resource_to_uri)]
+    p_a: Annotated[
+        Resource["B_Circular"],
+        Triple(
+            URIRef("http://some-predicate-a"),
+            PropertyStatus.mandatory,
+            map_resource_to_uri,
+        ),
+    ]
 
 
 class B_Circular(NewResource):
     p_b: Annotated[
-        Resource[A_Circular], Triple(URIRef("http://some-predicate-b"), PropertyStatus.mandatory,
-                                     map_resource_to_uri)]
+        Resource[A_Circular],
+        Triple(
+            URIRef("http://some-predicate-b"),
+            PropertyStatus.mandatory,
+            map_resource_to_uri,
+        ),
+    ]
+
+
+def test_multiple_triples_same_attr_serialised():
+    """
+    Test that we can specify multiple triples against a single attribute and that all of those triples get serialised.
+    """
+
+    class A(NewResource):
+        p: Annotated[
+            str,
+            Triple(
+                URIRef("http://first-predicate-uri"),
+                PropertyStatus.optional,
+                map_str_to_en_literal,
+            ),
+            Triple(
+                URIRef("http://second-predicate-uri"),
+                PropertyStatus.optional,
+                map_str_to_en_literal,
+            ),
+        ]
+
+    a = A("http://some-uri-a")
+    a.p = "Hello, World"
+    graph = a.to_graph(Graph())
+
+    assert 2 == len(graph)
+
+    assert (
+        URIRef("http://some-uri-a"),
+        URIRef("http://first-predicate-uri"),
+        Literal("Hello, World", "en"),
+    ) in graph
+
+    assert (
+        URIRef("http://some-uri-a"),
+        URIRef("http://second-predicate-uri"),
+        Literal("Hello, World", "en"),
+    ) in graph
+
+
+def test_inverse_triple_serialisation():
+    """Test that an inverse triple annotation is serialised correctly."""
+
+    class A(NewResource):
+        p: Annotated[
+            ExistingResource,
+            InverseTriple(
+                URIRef("http://some-predicate-uri"),
+                PropertyStatus.optional,
+                map_resource_to_uri,
+            ),
+        ]
+
+    a = A("http://some-uri-a")
+    a.p = ExistingResource("http://some-existing-resource-p")
+    graph = a.to_graph(Graph())
+
+    assert 1 == len(graph)
+    assert (
+        URIRef("http://some-existing-resource-p"),
+        URIRef("http://some-predicate-uri"),
+        URIRef("http://some-uri-a"),
+    )
+
+
+def test_mandatory_properties_are_required():
+    class A(NewResource):
+        p: Annotated[
+            str,
+            Triple(
+                URIRef("http://some-predicate-uri"),
+                PropertyStatus.mandatory,
+                map_str_to_en_literal,
+            ),
+        ]
+
+    a = A("http://some-entity-uri")
+
+    with pytest.raises(Exception) as ex:
+        a.to_graph(Graph())
+    assert "Mandatory RDF property" in str(ex.value)
 
 
 if __name__ == "__main__":
