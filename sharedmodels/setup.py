3894f8dd5031f3e3064f41378bbb77e8818496d2
Rob Barry <rob.barry@ons.gov.uk>
2021-09-14 12:15:14 +0100

    Locking to rdflib 5.x.x to avoid dealing with breaking changes.

--- sharedmodels/setup.py
+++ sharedmodels/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    install_requires=["unidecode", "rdflib>=5.0.0"],
+    install_requires=["unidecode", "rdflib~=5.0.0"],
     name="sharedmodels",
     version="0.0.1",
     packages=find_packages(exclude=["sharedmodels/scripts", "**/build"]),
