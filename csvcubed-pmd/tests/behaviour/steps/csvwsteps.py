168fecc090f7c10aa1e38bafb3e7cc180bc553a3
Rob Barry <rob.barry@ons.gov.uk>
2021-11-08 15:41:27 +0000

    Just one more reference to remove?

--- csvcubed-pmd/tests/behaviour/steps/csvwsteps.py
+++ csvcubed-pmd/tests/behaviour/steps/csvwsteps.py
@@ -1,8 +1,5 @@
 from behave import Given, When
 
-from csvcubedpmd.codelistcli.datetimecodelistgen import (
-    generate_date_time_code_lists_for_csvw_metadata_file,
-)
 from csvcubeddevtools.behaviour.file import get_context_temp_dir_path
 
 
@@ -11,11 +8,3 @@ def step_impl(context, file: str):
     temp_dir = get_context_temp_dir_path(context)
     context.csvw_path = temp_dir / file
     context.csvqb_path = temp_dir / file
-
-
-@When("a date/time code lists is generated from the CSV-qb")
-def step_impl(context):
-    temp_dir = get_context_temp_dir_path(context)
-    generate_date_time_code_lists_for_csvw_metadata_file(
-        context.csvqb_path.absolute(), output_directory=temp_dir
-    )
