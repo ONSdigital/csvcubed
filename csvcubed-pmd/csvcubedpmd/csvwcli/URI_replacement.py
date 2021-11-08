4e6282459e672510b24367e6d9e48fbd16f033b6
MuazzamChaud <92021581+MuazzamChaud@users.noreply.github.com>
2021-11-08 14:18:09 +0000

    first iteration of replacing URIs in ttl files

--- csvcubed-pmd/csvcubedpmd/csvwcli/URI_replacement.py
+++ csvcubed-pmd/csvcubedpmd/csvwcli/URI_replacement.py
@@ -1,1 +1,8 @@
+import click
+
+@click.command()
+@click.argument('turtle_test_file.ttl')
+def touch(turtle_test_file.ttl):
+    """Print turtle_test_file.ttl."""
+    click.echo(turtle_test_file.ttl)
 
