82fa1076a910e9ab3002ab543aeffc9fb2b81717
MuazzamChaud <92021581+MuazzamChaud@users.noreply.github.com>
2021-11-09 09:25:44 +0000

    Create URI_replecment.py

---
+++ csvcubed-pmd/csvcubedpmd/csvwcli/URI_replecment.py
@@ -0,0 +1,7 @@
+import click
+
+@click.command()
+@click.argument('src', envvar='SRC', type=click.File('r'))
+def echo(src):
+    """Print value of SRC environment variable."""
+    click.echo(src.read())
