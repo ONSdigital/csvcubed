199be729d2960386c07cb7b45b10e268cb1f6b8c
Auto-version-incrementer <none@none.com>
2021-11-09 10:06:04 +0000

    k

--- csvcubed-pmd/csvcubedpmd/csvwcli/URI_replecment.py
+++ csvcubed-pmd/csvcubedpmd/csvwcli/URI_replecment.py
@@ -1,7 +1,7 @@
 import click
 
 @click.command()
-@click.argument('src', envvar='SRC', type=click.File('turtle_test_file.ttl'))
+@click.argument('src', envvar='SRC', type=click.File('r'))
 def echo(src):
     """Print value of SRC environment variable."""
-    click.echo(src.read())
+    click.echo(src.read(turtle_test_file.ttl))
