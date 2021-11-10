9f13ed098015e4d56dfcd4737344c254fd9de9a1
nobody <none@none.com>
2021-11-10 13:42:19 +0000

    k

--- csvcubed-pmd/csvcubedpmd/main.py
+++ csvcubed-pmd/csvcubedpmd/main.py
@@ -6,16 +6,9 @@ import click
 
 
 from csvcubedpmd import csvwcli
-from csvcubedpmd.csvwcli import URI_replacement__init__
 
 entry_point = click.Group(
     commands=[csvwcli.csvw_group],
     help="pmdutils - Utils for helping convert CSV-Ws to PMD-compatible RDF.",
-    context_settings=dict(help_option_names=["-h", "--help"]),
-)
-
-entry_point2 = click.Group(
-    commands=[URI_replacement__init__.uri_group],
-    help="find and replece URIs in ttl files.",
     context_settings=dict(help_option_names=["-h", "--help"]),
 )
