3721bcb5bb8ee31abad1a8bdd5760ffa4825c7a0
nobody <none@none.com>
2021-11-11 09:17:40 +0000

    k

--- csvcubed-pmd/csvcubedpmd/csvwcli/URI_replecment.py
+++ csvcubed-pmd/csvcubedpmd/csvwcli/URI_replecment.py
@@ -1,14 +1,14 @@
 import click
 
 def replace():
-click.File(mode='r', encoding=None, errors='strict', lazy=None, atomic=False)
-@click.command()
-@click.argument('input', type=click.File('rb'))
-@click.argument('output', type=click.File('wb'))
-def inout(input, output):
-    """Copy contents of INPUT to OUTPUT."""
-    while True:
-        chunk = input.read(1024)
-        if not chunk:
-            break
-        output.write(chunk)
+    click.File(mode='r', encoding=None, errors='strict', lazy=None, atomic=False)
+    @click.command()
+    @click.argument('input', type=click.File('rb'))
+    @click.argument('output', type=click.File('wb'))
+    def inout(input, output):
+        """Copy contents of INPUT to OUTPUT."""
+        while True:
+            chunk = input.read(1024)
+            if not chunk:
+                break
+            output.write(chunk)
