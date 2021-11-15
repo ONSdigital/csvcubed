82a944188a394dcc0c90676d59a41a4dd96a4c54
nobody <none@none.com>
2021-11-15 11:50:50 +0000

    k

--- csvcubed-pmd/csvcubedpmd/urireplacement/urireplacement.py
+++ csvcubed-pmd/csvcubedpmd/urireplacement/urireplacement.py
@@ -1,4 +1,7 @@
 import click
+import urllib.request
+from chardet.universaldetector import UniversalDetector
+import logging
 
 def _line_check(line: str) -> bool:
     """
@@ -16,30 +19,39 @@ def _line_replace(line: str, values: tuple[tuple[str, str]]) -> str:
         print(f"replacing [{find}] with [{replace}]")
         line = line.replace(find, replace)
     print(f"New line: {line}")
-    # for paireduris in values:
-    #     whichuri = values[paireduris]
-    #     if whichuri[0] in line:
-    #         line.replace(whichuri[0],whichuri[1])
-    #         print(line)
-
-                
+    _line_check(line)
+    
     return line
+
+def _chardet(input: click.File):
+    detector = UniversalDetector()
+    for line in input.readlines():
+        detector.feed(line)
+        if detector.done: break
+    detector.close()
+    print(detector.result)
+    encodingtype = detector.result['encoding']
+    return encodingtype
 
 def _replace(input: click.File, output: click.File, values: tuple[tuple[str, str]]) -> None:
     """
     Docstring goes here
     """
-
+    encodingtype = _chardet(input)
+    print(encodingtype)
+    
     "Get character encoding type as a variable"
-    while True:
-        chunk = input.readline()
-        line = (input.readline()).decode()
+    #while True:
+    for index, line in enumerate(input):
+        line = (input.readline()).decode(encodingtype)
         "Add reading the line and decoding as one statement - it's no longer a chunk, maybe try would work here?"
         if not line:
             break
         _line_replace(line,values)
+        if _line_check(line):
+            logging.warning(f"remiaining 'file:/' URIs found on line {index}: {line}")
         "encode it back to that bytestream"
-        line = line.encode()
+        line = line.encode(encodingtype)
         output.write(line)
         print(line)
         print(type(values))
