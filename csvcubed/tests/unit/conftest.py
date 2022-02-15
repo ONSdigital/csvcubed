f2153e850f472522e545a4ee6444aa3590927135
Auto-version-incrementer <none@none.com>
2022-02-15 11:51:06 +0000

    fixed and configured failing pytests

--- csvcubed/tests/unit/conftest.py
+++ csvcubed/tests/unit/conftest.py
@@ -9,4 +9,11 @@ from tests.behaviour import *
 
 @pytest.fixture(autouse=True, scope="session")
 def configure_logging():
-    start_logging(console_level=logging.DEBUG)
+    start_logging(selected_logging_level = 'err')
+    start_logging(selected_logging_level = '')
+    start_logging(selected_logging_level = 'hello')
+    start_logging(selected_logging_level = None)
+    start_logging(selected_logging_level = 3)
+    logginglvl:int = 5
+    start_logging(selected_logging_level = logginglvl)
+
