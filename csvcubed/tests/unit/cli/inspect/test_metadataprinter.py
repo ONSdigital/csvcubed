efc147583f251028ff3a18087f22529bb955faa9
Gishan Don Ranasinghe <>
2022-02-22 18:05:23 +0000

    Updates to unit tests

--- csvcubed/tests/unit/cli/inspect/test_metadataprinter.py
+++ csvcubed/tests/unit/cli/inspect/test_metadataprinter.py
@@ -103,30 +103,3 @@ def test_csvw_catalog_metadata_printable_codelist():
         printable
         == "\u2022 The code list has the following catalog metadata: - Title: Alcohol Content- Label: Alcohol Content- Issued: 2022-02-11T21:00:21.040987- Modified: 2022-02-11T21:00:21.040987- License: None- Creator: None- Publisher: None- Landing Pages: None- Themes: None- Keywords: None- Contact Point: None- Identifier: Alcohol Content- Comment: None- Description: None"
     )
-
-
-def test_csvw_dsd_info_printable_datacube():
-    """
-    Should contain dataset label, qube components and cols with suppress output for the given data cube.
-    """
-    csvw_metadata_json_path = _test_case_base_dir / "datacube.csv-metadata.json"
-    metadata_processor = MetadataProcessor(csvw_metadata_json_path)
-    csvw_metadata_rdf_graph = metadata_processor.load_json_ld_to_rdflib_graph()
-    csvw_metadata_rdf_validator = MetadataValidator(csvw_metadata_rdf_graph)
-
-    (
-        _,
-        csvw_type,
-    ) = csvw_metadata_rdf_validator.validate_and_detect_type()
-
-    metadata_printer = MetadataPrinter(
-        csvw_type, csvw_metadata_rdf_graph, csvw_metadata_json_path
-    )
-    printable = metadata_printer.gen_dsd_info_printable()
-    printable = printable.replace("\t", "").replace("\n", "")
-
-    print(printable)
-    assert (
-        printable
-        == "• The data cube has the following data structure definition: - Dataset label: Alcohol Bulletin- Columns with suppress output: None- Number of components: 17- Components:                                                                                                         Property   Property Label Property Type Column Title Required                                                        http://purl.org/linked-data/sdmx/2009/dimension#refPeriod             None     Dimension         None     None                                                 http://gss-data.org.uk/def/trade/property/dimension/alcohol-type             None     Dimension         None     Nonefile:///workspaces/csvcubed/csvcubed/tests/test-cases/cli/inspect/alcohol-bulletin.csv#dimension/alcohol-sub-type Alcohol Sub Type     Dimension         None     None file:///workspaces/csvcubed/csvcubed/tests/test-cases/cli/inspect/alcohol-bulletin.csv#dimension/alcohol-content  Alcohol Content     Dimension         None     Nonefile:///workspaces/csvcubed/csvcubed/tests/test-cases/cli/inspect/alcohol-bulletin.csv#dimension/clearance-origin Clearance Origin     Dimension         None     None                                                                     http://purl.org/linked-data/cube#measureType             None     Dimension         None     None                                                         http://gss-data.org.uk/def/measure/alcohol-duty-receipts             None       Measure         None     None                                                            http://gss-data.org.uk/def/measure/beer-duty-receipts             None       Measure         None     None                                                           http://gss-data.org.uk/def/measure/cider-duty-receipts             None       Measure         None     None                                                                    http://gss-data.org.uk/def/measure/clearances             None       Measure         None     None                                                         http://gss-data.org.uk/def/measure/clearances-of-alcohol             None       Measure         None     None                                                             http://gss-data.org.uk/def/measure/production-volume             None       Measure         None     None                                                     http://gss-data.org.uk/def/measure/production-volume-alcohol             None       Measure         None     None                                                         http://gss-data.org.uk/def/measure/spirits-duty-receipts             None       Measure         None     None                                                            http://gss-data.org.uk/def/measure/wine-duty-receipts             None       Measure         None     None                                                      http://purl.org/linked-data/sdmx/2009/attribute#unitMeasure             None     Attribute         None     None                                                        http://purl.org/linked-data/sdmx/2009/attribute#obsStatus             None     Attribute         None     None"
-    )
