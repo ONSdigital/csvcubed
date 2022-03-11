3235c63ca0f7af32477a3f4a280b73d5fe8fe06e
Auto-version-incrementer <none@none.com>
2022-03-11 11:23:07 +0000

    splitting upc code into smaller functions

--- csvcubed/csvcubed/readers/preconfiguredtemplates.py
+++ csvcubed/csvcubed/readers/preconfiguredtemplates.py
@@ -4,14 +4,58 @@ Pre-configured JSON Templates
 
 Functionality to help augment JSON files with configuration from some pre-configured templates.
 """
-import json
 import logging
 
 from typing import Dict, Any
+from requests.exceptions import JSONDecodeError
 
 from csvcubed.utils.cache import session
 
+def get_template_file_from_template_lookup(template_value: str, version_module_path: str) -> str:
+    """
+    Given the `from_template` value, look up the template in the git repo
+    """
+
+    template_lookup_url =  f"https://raw.githubusercontent.com/GSS-Cogs/csvcubed/main/csvcubed/csvcubed/readers/{version_module_path}/templates/preset_column_config.json"
+    template_lookup_response = session.get(template_lookup_url)
+    logging.debug("The template is at: %s", template_lookup_url)
+   
+    if template_lookup_response.status_code != 200:
+        logging.warning("Status code: %s.", template_lookup_response.status_code)
+
+    try:
+        template_lookup = template_lookup_response.json()
+    except JSONDecodeError as e:
+        raise Exception(f"Could not access template lookup file at {template_lookup_url}") from e
+
+    template_file = template_lookup.get(template_value)
+
+    return template_file
+
+
+def get_propeties_from_template_file(template_file: str, version_module_path: str) -> dict:
+    """
+    Given the file path to the template, read in all the propeties of that particular template
+    """
+    template_template_lookup_url = f"https://raw.githubusercontent.com/GSS-Cogs/csvcubed/main/csvcubed/csvcubed/readers/{version_module_path}/templates/{template_file}"
+    template_response = session.get(template_template_lookup_url)
+    logging.critical(template_response)
+    if template_response.status_code != 200:
+        logging.warning("Status code: %s.", template_response.status_code)
+
+    try:
+        fetch_template = template_response.json() 
+    except JSONDecodeError as e:
+        raise Exception(f"Could not access template at {template_template_lookup_url}") from e
+
+    return fetch_template
+
+
 def apply_preconfigured_values_from_template(column_config: Dict[str, Any], version_module_path: str) -> None:
+    """
+    Preset templates are found through template lookup file. Propeties are then taken from templates and 
+    added to column config, with user specified propeties overriding template propeties.
+    """
     # if column_config doesn't have the `from_template` property, just terminate the function now
     if "from_template" not in column_config:
         return
@@ -22,23 +66,13 @@ def apply_preconfigured_values_from_template(column_config: Dict[str, Any], vers
     del column_config["from_template"]
 
     # given the `from_template` value, look up the template in the git repo
-    url =  f"https://raw.githubusercontent.com/GSS-Cogs/csvcubed/main/csvcubed/csvcubed/readers/{version_module_path}/templates/preset_column_config.json"
-    url_response = session.get(url)
-    logging.debug("The template is at: ", url)
-   
-    if url_response.status_code != 200:
-        logging.error("Status code: %s. Couldn't retrieve propeties from template at: %s" % (url_response.status_code, url))
+    template_file = get_template_file_from_template_lookup(template_value, version_module_path)
 
-    template_file = url_response.json().get(template_value)
-    template_url = f"https://raw.githubusercontent.com/GSS-Cogs/csvcubed/main/csvcubed/csvcubed/readers/{version_module_path}/templates/{template_file}"
-    template_response = session.get(template_url)
-    logging.critical(template_response)
-    if template_response.status_code != 200:
-        logging.error("Status code: %s. Couldn't retrieve propeties from template at: %s" % (template_response.status_code, url))
+    # given the file path to the template, read in all the propeties of that particular template
+    fetch_template = get_propeties_from_template_file(template_file, version_module_path)
 
-    # insert values frSnto column_config (as long as . Che user hasn't provided an overriding value for them)
-    read_template = json.loads(template_response.text) 
-    for key in read_template:
-        if key not in column_config:
-            column_config[key] = read_template[key]
+    # insert values from column_config (as long as the user hasn't provided an overriding value for them)
+    for propety in fetch_template:
+        if propety not in column_config:
+            column_config[propety] = fetch_template[propety]
     
