b71673f4b8b4e2f1a1226b7ad8669f9b5c49e545
Gishan Don Ranasinghe <>
2022-03-24 12:00:31 +0000

    Added not implemented error for codelist dataset url

--- csvcubed/csvcubed/cli/inspect/inspectsparqlmanager.py
+++ csvcubed/csvcubed/cli/inspect/inspectsparqlmanager.py
@@ -282,7 +282,8 @@ def select_codelist_dataset_url(rdf_graph: Graph) -> DatasetURLResult:
 
     :return: `DatasetURLResult`
     """
-    # TODO: Currently 0 results are returned. But it should return one result after implementing the loading of table schemas into rdf graph.
+    # TODO As part of CodeList info display ticket: Currently 0 results are returned. But it should return one result.
+    raise NotImplementedError
 
     results: List[ResultRow] = select(
         _get_query_string_from_file(SPARQLQueryFileName.SELECT_CODELIST_DATASET_URL),
