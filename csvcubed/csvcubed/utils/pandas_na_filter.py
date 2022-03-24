8b47b794ee2f0ae9d2795a3c3bf29b0d5bbe33ad
Auto-version-incrementer <none@none.com>
2022-03-24 09:51:46 +0000

    first iteration of solution

---
+++ csvcubed/csvcubed/utils/pandas_na_filter.py
@@ -0,0 +1,24 @@
+import logging
+from typing import Optional
+import pandas as pd
+
+from pathlib import Path
+
+def refiltered_pandas_read_csv(csv: Path) ->  pd.DataFrame:
+
+    specified_na_values = _get_new_na_values()
+    df = pd.read_csv(csv, keep_default_na = False, na_values = specified_na_values)
+
+    # Replace "NaN" with "Great"
+    # df = df.fillna("Great")
+    return df
+
+def _get_new_na_values():
+    specified_na_values = {
+    "",
+    }
+    return specified_na_values
+
+csv_path = "/workspaces/csvcubed/csvcubed/tests/test-cases/utils/pandas_na_filter/code-list.csv"
+logging.critical(refiltered_pandas_read_csv(csv_path))
+logging.error(pd.read_csv(csv_path))
---
+++ csvcubed/tests/unit/utils/test_pandas_na_filter.py
@@ -0,0 +1,28 @@
+import pytest
+from pathlib import Path
+
+from csvcubed.utils.pandas_na_filter import refiltered_pandas_read_csv
+
+csv_path = "/workspaces/csvcubed/csvcubed/tests/test-cases/utils/pandas_na_filter/code-list.csv"
+
+def test_na_filter_works():
+    pd_table = refiltered_pandas_read_csv(csv_path)
+    assert "nan" in pd_table['Description'].values
+    assert "null" in pd_table['Description'].values
+    assert "#N/A" not in pd_table['Description'].values
+    assert "null"  not in pd_table['Parent Notation'].values
+    assert "NULL" in pd_table['Parent Notation'].values
+    assert "-nan" in pd_table['Parent Notation'].values
+    assert "" not in pd_table
+    assert "" not in pd_table["Parent Notation"].values
+    assert "" not in pd_table["Description"].values
+    assert "NaN" not in pd_table["Parent Notation"].values
+
+def test_empty_strings_are_replaced_with_hello():
+    pd_table = refiltered_pandas_read_csv(csv_path)
+    assert None in pd_table["Description"].values
+    pd_table = pd_table.fillna("hello")
+    assert "hello" in pd_table["Description"].values
+
+if __name__ == "__main__":
+    pytest.main()
