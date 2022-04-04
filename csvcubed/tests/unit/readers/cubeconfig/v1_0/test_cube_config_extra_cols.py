f0d7afec54d80d9278433d7980221e381376dee2
Rob Barry <rob.barry@ons.gov.uk>
2022-04-04 14:11:02 +0000

    Stop creating more of those unnecessary 'validation_errors.json' and 'out' file/folders when unit testing.

--- csvcubed/tests/unit/readers/cubeconfig/v1_0/test_cube_config_extra_cols.py
+++ csvcubed/tests/unit/readers/cubeconfig/v1_0/test_cube_config_extra_cols.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from tempfile import TemporaryDirectory
 
 import pytest
 
@@ -11,35 +12,39 @@ TEST_CASE_DIR = get_test_cases_dir().absolute() / "readers" / "cube-config" / "v
 
 @pytest.mark.vcr
 def test_build_with_fail():
-    config = Path(TEST_CASE_DIR, "cube_data_config_extra_cols.json")
-    output = Path("./out")
-    csv = Path(TEST_CASE_DIR, "cube_data_config_extra_cols.csv")
-    with pytest.raises(SystemExit):
-        cli_build(
-            config_path=config,
-            output_directory=output,
-            csv_path=csv,
-            fail_when_validation_error_occurs=True,
-            validation_errors_file_out=Path(output, "validation_errors.json"),
-        )
+    with TemporaryDirectory() as temp_dir:
+        temp_dir = Path(temp_dir)
+        config = Path(TEST_CASE_DIR, "cube_data_config_extra_cols.json")
+        output = temp_dir / "out"
+        csv = Path(TEST_CASE_DIR, "cube_data_config_extra_cols.csv")
+        with pytest.raises(SystemExit):
+            cli_build(
+                config_path=config,
+                output_directory=output,
+                csv_path=csv,
+                fail_when_validation_error_occurs=True,
+                validation_errors_file_out=Path(output, "validation_errors.json"),
+            )
 
 
 @pytest.mark.vcr
 def test_build_without_fail():
-    config = Path(TEST_CASE_DIR, "cube_data_config_ok.json")
-    output = Path("./out")
-    csv = Path(TEST_CASE_DIR, "cube_data_config_ok.csv")
-    cube, errors = cli_build(
-        config_path=config,
-        output_directory=output,
-        csv_path=csv,
-        fail_when_validation_error_occurs=False,
-        validation_errors_file_out=Path(output, "validation_errors.json"),
-    )
-    assert isinstance(cube, Cube)
-    assert isinstance(errors, list)
-    output_path = Path(output, "validation_errors.json").resolve()
-    assert output_path.exists()
+    with TemporaryDirectory() as temp_dir:
+        temp_dir = Path(temp_dir)
+        config = Path(TEST_CASE_DIR, "cube_data_config_ok.json")
+        output = temp_dir / "out"
+        csv = Path(TEST_CASE_DIR, "cube_data_config_ok.csv")
+        cube, errors = cli_build(
+            config_path=config,
+            output_directory=output,
+            csv_path=csv,
+            fail_when_validation_error_occurs=False,
+            validation_errors_file_out=Path(output, "validation_errors.json"),
+        )
+        assert isinstance(cube, Cube)
+        assert isinstance(errors, list)
+        output_path = Path(output, "validation_errors.json").resolve()
+        assert output_path.exists()
 
 
 if __name__ == "__main__":
