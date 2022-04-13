b9ffc44b057492f6ec91b122541f8575f9bade6e
Gishan Don Ranasinghe <99191124+GDonRanasinghe@users.noreply.github.com>
2022-04-13 11:31:21 +0100

    392 refactor inspect metadata printer class (#424)
    * WIP metadata printer refactor
    
    * Updates to the metadata printer
    
    * Fixes for metadata printer class refactor
    
    * Removed yaml
    
    * Removed prints
    
    * Updates to imports
    
    * Spelling, style, all minor updates
    
    Co-authored-by: Gishan Don Ranasinghe <>
    Co-authored-by: Andrew Fergusson <andrew.fergusson@ons.gov.uk>

--- csvcubed/csvcubed/cli/inspect/metadataprinter.py
+++ csvcubed/csvcubed/cli/inspect/metadataprinter.py
@@ -7,18 +7,18 @@ Provides functionality for validating and detecting input metadata.json file.
 
 from dataclasses import dataclass, field
 from pathlib import Path
-from typing import List
-import pandas as pd
+from typing import List, Optional, Tuple
+from pandas import DataFrame
 
 from rdflib import Graph
 
 from csvcubed.models.inspectsparqlresults import (
     CatalogMetadataResult,
+    CodeListColsByDatasetUrlResult,
+    CodelistColumnResult,
     CodelistsResult,
     ColsWithSuppressOutputTrueResult,
     DSDLabelURIResult,
-    DatasetURLResult,
-    QubeComponentResult,
     QubeComponentsResult,
 )
 from csvcubed.cli.inspect.metadatainputvalidator import CSVWType
@@ -39,6 +39,8 @@ from csvcubed.cli.inspect.inspectdatasetmanager import (
     load_csv_to_dataframe,
 )
 from csvcubed.models.inspectdataframeresults import (
+    CodelistHierarchyInfoResult,
+    DatasetObservationsByMeasureUnitInfoResult,
     DatasetObservationsInfoResult,
 )
 from csvcubed.utils.csvdataset import (
@@ -64,22 +66,152 @@ class MetadataPrinter:
     csvw_metadata_rdf_graph: Graph
     csvw_metadata_json_path: Path
 
-    dataset_uri: str = field(init=False)
-    dsd_uri: str = field(init=False)
-    qube_components: List[QubeComponentResult] = field(init=False)
+    csvw_type_str: str = field(init=False)
+    dataset_url: str = field(init=False)
+    dataset: DataFrame = field(init=False)
 
-    def _get_type_str(self):
-        if self.csvw_type == CSVWType.QbDataSet:
+    result_catalog_metadata: CatalogMetadataResult = field(init=False)
+    result_dataset_label_dsd_uri: DSDLabelURIResult = field(init=False)
+    result_qube_components: QubeComponentsResult = field(init=False)
+    result_cols_with_suppress_output_true: ColsWithSuppressOutputTrueResult = field(
+        init=False
+    )
+    result_code_lists: CodelistsResult = field(init=False)
+    result_dataset_observations_info: DatasetObservationsInfoResult = field(init=False)
+    result_dataset_value_counts: DatasetObservationsByMeasureUnitInfoResult = field(
+        init=False
+    )
+    result_code_list_cols: CodeListColsByDatasetUrlResult = field(init=False)
+    result_concepts_hierachy_info: CodelistHierarchyInfoResult = field(init=False)
+
+    @staticmethod
+    def get_csvw_type_str(csvw_type: CSVWType) -> str:
+        if csvw_type == CSVWType.QbDataSet:
             return "data cube"
-        elif self.csvw_type == CSVWType.CodeList:
+        elif csvw_type == CSVWType.CodeList:
             return "code list"
         else:
             raise InputTypeIsUnknownException()
 
+    @staticmethod
+    def get_dataset_url(
+        csvw_metadata_rdf_graph: Graph,
+        csvw_type: CSVWType,
+        dataset_uri: str,
+    ) -> str:
+        if csvw_type == CSVWType.QbDataSet:
+            return select_qb_dataset_url(
+                csvw_metadata_rdf_graph, dataset_uri
+            ).dataset_url
+        elif csvw_type == CSVWType.CodeList:
+            return select_codelist_dataset_url(csvw_metadata_rdf_graph).dataset_url
+        else:
+            raise JsonldNotSupportedException()
+
+    @staticmethod
+    def get_parent_label_notation_col_names(
+        columns: List[CodelistColumnResult],
+    ) -> Tuple[Optional[str], Optional[str], Optional[str]]:
+        parent_notation_col_name = get_codelist_col_title_by_property_url(
+            columns, CodelistPropertyUrl.SkosBroader
+        )
+        label_col_name = get_codelist_col_title_by_property_url(
+            columns, CodelistPropertyUrl.RDFLabel
+        )
+        notation_col_name = get_codelist_col_title_by_property_url(
+            columns, CodelistPropertyUrl.SkosNotation
+        )
+        return (parent_notation_col_name, label_col_name, notation_col_name)
+
+    def generate_general_results(self):
+        """
+        Generates results related to data cubes and code lists.
+
+        Member of :class:`./MetadataPrinter`.
+        """
+        self.csvw_type_str = self.get_csvw_type_str(self.csvw_type)
+        self.result_catalog_metadata = select_csvw_catalog_metadata(
+            self.csvw_metadata_rdf_graph
+        )
+        self.dataset_url = self.get_dataset_url(
+            self.csvw_metadata_rdf_graph,
+            self.csvw_type,
+            self.result_catalog_metadata.dataset_uri,
+        )
+        self.dataset = load_csv_to_dataframe(
+            self.csvw_metadata_json_path, Path(self.dataset_url)
+        )
+        self.result_dataset_observations_info = get_dataset_observations_info(
+            self.dataset, self.csvw_type
+        )
+
+    def get_datacube_results(self):
+        """
+        Generates results specific to data cubes.
+
+        Member of :class:`./MetadataPrinter`.
+        """
+        self.result_dataset_label_dsd_uri = (
+            select_csvw_dsd_dataset_label_and_dsd_def_uri(self.csvw_metadata_rdf_graph)
+        )
+        self.result_qube_components = select_csvw_dsd_qube_components(
+            self.csvw_metadata_rdf_graph,
+            self.result_dataset_label_dsd_uri.dsd_uri,
+            self.csvw_metadata_json_path,
+        )
+        self.result_cols_with_suppress_output_true = (
+            select_cols_where_supress_output_is_true(self.csvw_metadata_rdf_graph)
+        )
+        self.result_code_lists = select_dsd_code_list_and_cols(
+            self.csvw_metadata_rdf_graph,
+            self.result_dataset_label_dsd_uri.dsd_uri,
+            self.csvw_metadata_json_path,
+        )
+        (
+            canonical_shape_dataset,
+            measure_col,
+            unit_col,
+        ) = transform_dataset_to_canonical_shape(
+            self.dataset,
+            self.result_qube_components.qube_components,
+            self.result_dataset_label_dsd_uri.dsd_uri,
+            self.csvw_metadata_rdf_graph,
+            self.csvw_metadata_json_path,
+        )
+        self.result_dataset_value_counts = get_dataset_val_counts_info(
+            canonical_shape_dataset, measure_col, unit_col
+        )
+
+    def generate_codelist_results(self):
+        """
+        Generates results specific to code lists.
+
+        Member of :class:`./MetadataPrinter`.
+        """
+        self.result_code_list_cols = select_codelist_cols_by_dataset_url(
+            self.csvw_metadata_rdf_graph, self.dataset_url
+        )
+        (
+            parent_notation_col_name,
+            label_col_name,
+            notation_col_name,
+        ) = self.get_parent_label_notation_col_names(self.result_code_list_cols.columns)
+        self.result_concepts_hierachy_info = get_concepts_hierarchy_info(
+            self.dataset, parent_notation_col_name, label_col_name, notation_col_name
+        )
+
+    def __post_init__(self):
+        self.generate_general_results()
+
+        if self.csvw_type == CSVWType.QbDataSet:
+            self.get_datacube_results()
+        elif self.csvw_type == CSVWType.CodeList:
+            self.generate_codelist_results()
+
     @property
     def type_info_printable(self) -> str:
         """
-        Generates a printable of metadata type information.
+        Returns a printable of file content type.
 
         Member of :class:`./MetadataPrinter`.
 
@@ -93,137 +225,65 @@ class MetadataPrinter:
     @property
     def catalog_metadata_printable(self) -> str:
         """
-        Generates a printable of catalog metadata (e.g. title, description, etc.).
+        Returns a printable of catalog metadata (e.g. title, description).
 
         Member of :class:`./MetadataPrinter`.
 
         :return: `str` - user-friendly string which will be output to CLI.
         """
-        result: CatalogMetadataResult = select_csvw_catalog_metadata(
-            self.csvw_metadata_rdf_graph
-        )
-        self.dataset_uri = result.dataset_uri
-
-        return f"- The {self._get_type_str()} has the following catalog metadata:{result.output_str}"
+        return f"- The {self.csvw_type_str} has the following catalog metadata:{self.result_catalog_metadata.output_str}"
 
     @property
     def dsd_info_printable(self) -> str:
         """
-        Generates a printable of Data Structure Definition (DSD) information.
+        Returns a printable of data structure definition (DSD).
 
         Member of :class:`./MetadataPrinter`.
 
         :return: `str` - user-friendly string which will be output to CLI.
         """
-        result_dataset_label_dsd_uri: DSDLabelURIResult = (
-            select_csvw_dsd_dataset_label_and_dsd_def_uri(self.csvw_metadata_rdf_graph)
-        )
-        self.dsd_uri = result_dataset_label_dsd_uri.dsd_uri
-
-        result_qube_components: QubeComponentsResult = select_csvw_dsd_qube_components(
-            self.csvw_metadata_rdf_graph, self.dsd_uri, self.csvw_metadata_json_path
-        )
-        self.qube_components = result_qube_components.qube_components
-
-        result_cols_with_suppress_output_true: ColsWithSuppressOutputTrueResult = (
-            select_cols_where_supress_output_is_true(self.csvw_metadata_rdf_graph)
-        )
-
-        return f"- The {self._get_type_str()} has the following data structure definition:{result_dataset_label_dsd_uri.output_str}{result_qube_components.output_str}{result_cols_with_suppress_output_true.output_str}"
+        return f"- The {self.csvw_type_str} has the following data structure definition:{self.result_dataset_label_dsd_uri.output_str}{self.result_qube_components.output_str}{self.result_cols_with_suppress_output_true.output_str}"
 
     @property
     def codelist_info_printable(self) -> str:
         """
-        Generates a printable of dsd code list information (e.g. column name, type, etc.).
+        Returns a printable of data structure definition (DSD) code list information (e.g. column name, type, etc.).
 
         Member of :class:`./MetadataPrinter`.
 
         :return: `str` - user-friendly string which will be output to CLI.
         """
-
-        result: CodelistsResult = select_dsd_code_list_and_cols(
-            self.csvw_metadata_rdf_graph, self.dsd_uri, self.csvw_metadata_json_path
-        )
-
-        return f"- The {self._get_type_str()} has the following code list information:{result.output_str}"
+        return f"- The {self.csvw_type_str} has the following code list information:{self.result_code_lists.output_str}"
 
     @property
     def dataset_observations_info_printable(self) -> str:
         """
-        Generates a printable of top 10 and last 10 records.
+        Returns a printable of top 10 and last 10 records.
 
         Member of :class:`./MetadataPrinter`.
 
         :return: `str` - user-friendly string which will be output to CLI.
         """
-
-        result_dataset_url: DatasetURLResult
-        if self.csvw_type == CSVWType.QbDataSet:
-            result_dataset_url = select_qb_dataset_url(
-                self.csvw_metadata_rdf_graph, self.dataset_uri
-            )
-        elif self.csvw_type == CSVWType.CodeList:
-            result_dataset_url = select_codelist_dataset_url(
-                self.csvw_metadata_rdf_graph
-            )
-        else:
-            raise JsonldNotSupportedException()
-
-        self.dataset_url: str = result_dataset_url.dataset_url
-        self.dataset: pd.DataFrame = load_csv_to_dataframe(
-            self.csvw_metadata_json_path, Path(self.dataset_url)
-        )
-
-        result: DatasetObservationsInfoResult = get_dataset_observations_info(
-            self.dataset, self.csvw_type
-        )
-
-        return f"- The {self._get_type_str()} has the following dataset information:{result.output_str}"
+        return f"- The {self.csvw_type_str} has the following dataset information:{self.result_dataset_observations_info.output_str}"
 
     @property
     def dataset_val_counts_by_measure_unit_info_printable(self) -> str:
         """
-        Generates a printable of dataset value counts broken-down by measure and unit.
+        Returns a printable of data set value counts broken-down by measure and unit.
 
         Member of :class:`./MetadataPrinter`.
 
         :return: `str` - user-friendly string which will be output to CLI.
         """
-        (
-            canonical_shape_dataset,
-            measure_col,
-            unit_col,
-        ) = transform_dataset_to_canonical_shape(
-            self.dataset,
-            self.qube_components,
-            self.dsd_uri,
-            self.csvw_metadata_rdf_graph,
-            self.csvw_metadata_json_path,
-        )
-
-        result = get_dataset_val_counts_info(
-            canonical_shape_dataset, measure_col, unit_col
-        )
-        return f"- The {self._get_type_str()} has the following value counts:{result.output_str}"
+        return f"- The {self.csvw_type_str} has the following value counts:{self.result_dataset_value_counts.output_str}"
 
     @property
     def codelist_hierachy_info_printable(self) -> str:
-        result_code_list_cols = select_codelist_cols_by_dataset_url(
-            self.csvw_metadata_rdf_graph, self.dataset_url
-        )
+        """
+        Returns a printable of code list concepts hierarchy.
 
-        parent_notation_col_name = get_codelist_col_title_by_property_url(
-            result_code_list_cols.columns, CodelistPropertyUrl.SkosBroader
-        )
-        label_col_name = get_codelist_col_title_by_property_url(
-            result_code_list_cols.columns, CodelistPropertyUrl.RDFLabel
-        )
-        notation_col_name = get_codelist_col_title_by_property_url(
-            result_code_list_cols.columns, CodelistPropertyUrl.SkosNotation
-        )
+        Member of :class:`./MetadataPrinter`.
 
-        result = get_concepts_hierarchy_info(
-            self.dataset, parent_notation_col_name, label_col_name, notation_col_name
-        )
-
-        return f"- The {self._get_type_str()} has the following concepts information:{result.output_str}"
+        :return: `str` - user-friendly string which will be output to CLI.
+        """
+        return f"- The {self.csvw_type_str} has the following concepts information:{self.result_concepts_hierachy_info.output_str}"
