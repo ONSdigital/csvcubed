7564efdcc1596afd6d64500dc5e15b7a67f87d0d
Gishan Don Ranasinghe <>
2022-05-10 13:57:54 +0100

    Updates to schema version detection

--- csvcubed/csvcubed/readers/cubeconfig/schema_versions.py
+++ csvcubed/csvcubed/readers/cubeconfig/schema_versions.py
@@ -4,7 +4,7 @@ Schema Versions
 
 Contains an enum listing the qube-config.json schema versions recognised by csvcubed.
 """
-from enum import Enum
+from enum import Enum, auto
 from pathlib import Path
 from typing import Optional, Callable, Tuple, List
 
@@ -19,17 +19,25 @@ QubeConfigDeserialiser = Callable[
     Tuple[QbCube, List[JsonSchemaValidationError], List[ValidationError]],
 ]
 
-_V1_SCHEMA_URL = "https://purl.org/csv-cubed/qube-config/v1.0"
-_V1_1_SCHEMA_URL = "/workspaces/csvcubed/csvcubed/csvcubed/schema/cube-config/v1_1/schema.json"  # TODO: Chnage to the purl v1.1 url
+_v1_0_SCHEMA_URL = "https://purl.org/csv-cubed/qube-config/v1.0"
+_v1_1_SCHEMA_URL = "/workspaces/csvcubed/csvcubed/csvcubed/schema/cube-config/v1_1/schema.json"  # TODO: Chnage to the purl v1.1 url
 
 
-class QubeConfigJsonSchemaVersion(Enum):
+class QubeConfigJsonSchemaMinorVersion(Enum):
     """
-    Known versions of the QubeConfig JSON Schema and the directory/module name they are contained within.
+    Minor versions of the cube config schema.
     """
 
-    V1_0 = "v1_0"
-    V1_1 = "v1_1"
+    v0 = "0"
+    v1 = "1"
+
+
+class QubeConfigJsonSchemaMajourVersion(Enum):
+    """
+    Majour versions of the cube config schema.
+    """
+
+    v1 = "v1"
 
 
 def get_deserialiser_for_schema(
@@ -39,25 +47,31 @@ def get_deserialiser_for_schema(
     Provides a versioned deserialiser function appropriate to the referenced schema.
     """
     # Default to the latest version of the schema.
-    schema_path = _V1_SCHEMA_URL if maybe_schema_path is None else maybe_schema_path
+    schema_path = _v1_1_SCHEMA_URL if maybe_schema_path is None else maybe_schema_path
 
-    schema_version = _get_schema_version(schema_path)
-    
-    # TODO: Handle schema version detection
-    majour_version = 1
-
-    if majour_version == 1:
-        return v1.configdeserialiser.get_deserialiser(schema_path, schema_version.value)
-    # If there is a new majour version of the cube config schema, another elif needs adding. And a new version of the deseariler needs to be created. For more info, please visit https://purl.org/csv-cubed/schema-versioning
+    schema_version_majour, _ = _get_schema_version(schema_path)
+    if schema_version_majour == QubeConfigJsonSchemaMajourVersion.v1:
+        return v1.configdeserialiser.get_deserialiser(
+            schema_path, schema_version_majour.value
+        )
+    # If there is a new majour version of the cube config schema, another elif needs adding here. And a new version of the deseariler needs to be created. For more info, please visit https://purl.org/csv-cubed/schema-versioning
     else:
-        raise ValueError(f"Unhandled schema version {schema_version}")
+        raise ValueError(f"Unhandled schema version {schema_version_majour}")
 
 
-def _get_schema_version(schema_path: str) -> QubeConfigJsonSchemaVersion:
-    if schema_path == _V1_SCHEMA_URL:
-        return QubeConfigJsonSchemaVersion.V1_0
-    elif schema_path == _V1_1_SCHEMA_URL:
-        return QubeConfigJsonSchemaVersion.V1_1
+def _get_schema_version(
+    schema_path: str,
+) -> Tuple[QubeConfigJsonSchemaMajourVersion, QubeConfigJsonSchemaMinorVersion]:
+    if schema_path == _v1_0_SCHEMA_URL:
+        return (
+            QubeConfigJsonSchemaMajourVersion.v1,
+            QubeConfigJsonSchemaMinorVersion.v0,
+        )
+    elif schema_path == _v1_1_SCHEMA_URL:
+        return (
+            QubeConfigJsonSchemaMajourVersion.v1,
+            QubeConfigJsonSchemaMinorVersion.v1,
+        )
     else:
         raise ValueError(
             f"The $schema '{schema_path}' referenced in the cube config file is not recognised."
