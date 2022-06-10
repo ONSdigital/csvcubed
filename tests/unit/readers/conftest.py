7a56a11ed9e7d4bdeb6e10a3ed4bb2dea76fa9b8
Rob Barry <rob.barry@ons.gov.uk>
2022-06-10 09:01:31 +0100

    Correcting ROOT_DIR_PATH reference when inside whl package.

--- tests/unit/readers/conftest.py
+++ tests/unit/readers/conftest.py
@@ -2,7 +2,7 @@ import pytest
 import requests_mock
 
 from csvcubed.utils.cache import session
-from csvcubed.definitions import ROOT_DIR_PATH
+from csvcubed.definitions import APP_ROOT_DIR_PATH
 
 
 @pytest.fixture(autouse=True, scope="package")
@@ -15,13 +15,7 @@ def mock_http_session():
         session=session, real_http=True
     ) as mocker:
         templates_dir = (
-            ROOT_DIR_PATH
-            / "src"
-            / "csvcubed"
-            / "readers"
-            / "cubeconfig"
-            / "v1_0"
-            / "templates"
+            APP_ROOT_DIR_PATH / "readers" / "cubeconfig" / "v1_0" / "templates"
         )
 
         template_files = templates_dir.rglob("**/*.json*")
