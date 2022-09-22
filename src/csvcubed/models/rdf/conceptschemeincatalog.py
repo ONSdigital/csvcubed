5ec1c33796cf78a171d521b9e3a0e04e66ad3367
Rob Barry <rob.barry@ons.gov.uk>
2022-09-22 16:51:47 +0100

    Fix for problem. MRO + we were secrently overriding `was_generated_by` in dcat.Dataset.

--- src/csvcubed/models/rdf/conceptschemeincatalog.py
+++ src/csvcubed/models/rdf/conceptschemeincatalog.py
@@ -7,19 +7,21 @@ from typing import Annotated
 from csvcubedmodels.rdf import dcat, skos
 
 from csvcubed.models.rdf import prov
+
 # activity = prov_Activity("#activity-uri")
 # activity.used = ExistingResource("http://github.com/v0.1.4")
 
 # entity = prov_Entity("#code-list-identi")
 # entity.was_generated_by = activity
 
-class ConceptSchemeInCatalog(skos.ConceptScheme, dcat.Dataset, prov.Entity):
+
+class ConceptSchemeInCatalog(prov.Entity, skos.ConceptScheme, dcat.Dataset):
     """
     Represents both a skos:ConceptScheme and a dcat:Dataset in one node. Means that we don't have to link
     between the two.
     """
 
     def __init__(self, uri: str):
+        prov.Entity.__init__(self, uri)
         skos.ConceptScheme.__init__(self, uri)
         dcat.Dataset.__init__(self, uri)
-        prov.Entity.__init__(self, uri)
