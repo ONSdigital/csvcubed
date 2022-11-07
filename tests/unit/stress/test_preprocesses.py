de1f69a47fc3286a7ed6074d1bd798d5b45727ac
CharlesRendle <charles.rendle@ons.gov.uk>
2022-11-07 11:56:04 +0000

    Addresses PR commens #1

--- tests/unit/stress/test_preprocesses.py
+++ tests/unit/stress/test_preprocesses.py
@@ -2,14 +2,16 @@ from tempfile import TemporaryDirectory
 from pathlib import Path
 
 import pandas as pd
+from pandas.testing import assert_frame_equal
 import pytest
+from csvcubed.models.cube.qb.components.measure import ExistingQbMeasure
 
 from tests.stress.buildpreprocess import generate_maximally_complex_csv
 
 
 def test_generated_csv_exists():
     """
-        This function checks that `_generate_maximally_complex_csv` generates a csv file and check that the files exist at the desired location
+    This function checks that `_generate_maximally_complex_csv` generates a csv file and check that the files exist at the desired location
     """
     with TemporaryDirectory() as tmp:
 
@@ -18,7 +20,6 @@ def test_generated_csv_exists():
         numb_rows = 100
 
         generate_maximally_complex_csv(numb_rows, tmp_dir)
-
 
         assert (tmp_dir / "stress.csv").exists()
 
@@ -31,24 +32,27 @@ def test_generated_csv_shape_and_num_unique_values():
 
         tmp_dir = Path(tmp)
 
-        numb_rows = 50
+        numb_rows = 100
 
         generate_maximally_complex_csv(numb_rows, tmp_dir)
 
-        df = pd.read_csv(tmp_dir / "stress.csv")
+        existing_df = pd.read_csv("tests/test-cases/stress/stress.csv")
 
-        assert len(df.columns) == 17
+        generated_df = pd.read_csv(tmp_dir / "stress.csv")
 
-        for c in df.columns:
-            assert len(df[c]) == numb_rows, f"{c} does not have {numb_rows} values"
+        for c in generated_df.columns:
+            assert (
+                len(generated_df[c]) == numb_rows
+            ), f"{c} does not have {numb_rows} values"
             if c == "Measure":
-                assert len(df[c].unique()) == 20
+                assert len(generated_df[c].unique()) == 20
             else:
-                assert len(df[c].unique()) == numb_rows, f"{c} doesn't have {numb_rows} unique values"
-            
+                assert (
+                    len(generated_df[c].unique()) == numb_rows
+                ), f"{c} doesn't have {numb_rows} unique values"
+
+        assert_frame_equal(existing_df, generated_df)
+
 
 if __name__ == "__main__":
     pytest.main()
-        
-
-        
