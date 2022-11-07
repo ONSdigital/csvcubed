05bd9aae3eff0dbb33a0fee6a1865363b907e0cb
Rob Barry <rob.barry@ons.gov.uk>
2022-11-07 11:02:29 +0000

    Try this.

--- windows-testing-setup.ps1
+++ windows-testing-setup.ps1
@@ -38,7 +38,7 @@ $csv2rdfPath = (Get-Item csv2rdf.jar | Resolve-Path).Path.Substring(38)
 Set-Content -Path csv2rdf.bat -Value "@REM Forwarder script`n@echo off`necho Attempting to launch csv2rdf at $csv2rdfPath`njava -jar $csv2rdfPath %*" 
 
 $csv2rdfLocation = (Get-Item csv2rdf.bat | Resolve-Path).Path.Substring(38)
-echo "CSV2RDF_LOCATION=$csv2rdfLocation" | Out-File -FilePath $Env:GITHUB_OUTPUT -Encoding utf8 -Append
+echo "CSV2RDF_LOCATION=$csv2rdfLocation" | Out-File -FilePath $env:GITHUB_OUTPUT -Encoding utf8 -Append
 Write-Output = "csv2rdf location: $csv2rdfLocation"
 
 $path = "$path;$pwd"
@@ -56,7 +56,7 @@ git clone --depth 1 https://github.com/GSS-Cogs/gdp-sparql-tests.git
 
 $sparqlTestsLocation = (Get-Item gdp-sparql-tests/tests | Resolve-Path).Path.Substring(38)
 Write-Output = "Sparql test location: $sparqlTestsLocation"
-echo "SPARQL_TESTS_LOCATION=$sparqlTestsLocation" | Out-File -FilePath $Env:GITHUB_OUTPUT -Encoding utf8 -Append
+echo "SPARQL_TESTS_LOCATION=$sparqlTestsLocation" | Out-File -FilePath $env:GITHUB_OUTPUT -Encoding utf8 -Append
 
 Write-Output "Setting path"
 Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $path
