0d777fcebcbf427d84e4f9f2abf817fdc3117d2d
CharlesRendle <43996614+CharlesRendle@users.noreply.github.com>
2022-11-09 17:14:32 +0000

    Charles rendle/issue563 (#622)
    * Dump first commit
    
    * Jozsef test script + change to grrovy file
    
    * progress commit
    
    * modified the test.py
    
    * Progress commit monday morning!
    
    * progress commit monday afternoon.
    
    * Big structural change commit.
    
    * Merge branch 'CharlesRendle/issue563' of https://github.com/GSS-Cogs/csvcubed into CharlesRendle/issue563
    
    * deleted a load of not needed files
    
    * Progress commit 07.10.22 - Jozsef can fetch
    
    * Chnaged the path file for the stress test
    
    * Split up tests, tdl: run bulid in inspect pretest
    
    * Progress commit
    
    * Created the metrics converter script
    
    * Whole tests run from bash script! Cleanup imminent
    
    * Removes old attempts
    
    * Adds jmeter log + metrics to their own folders
    
    * Tidy of imports
    
    * Changes generated csv filename back to stress.csv
    
    * Unit testing files
    
    * changes to the preprocess - can start functionise
    
    * removes duplication in inspectpreprocess
    
    * changes made to the scripts to reduce duplication
    
    * Progress commit
    
    * Progress commit
    
    * commit before breaking anything else...
    
    * Stresstest working. tdl: diagnose time gaps
    
    * Fixes test by adding run_type to log title
    
    * Rounds printed values, edits readme, comment tests
    
    * Adds test .log file to test cases
    
    * Removes an unused variable name (uses _ instead)
    
    * Addresses PR commens #1
    
    * Changes import of test-case
    
    * Added installation script to readme. Iffy mkdwn
    
    * triple backtick for codeblock
    
    * Adds OS X distinction
    
    * Final commit as per PR
    
    * unsafe_hash=True for qbmeasure + unit
    
    * Removes colons from path and file names.
    
    * Changes the colon in expected test file name too
    
    Co-authored-by: Jozsef K <43893201+nimshi89@users.noreply.github.com>

---
+++ tests/stress/inspectpreprocess.py
@@ -0,0 +1,20 @@
+import csv
+import sys
+import os
+from pathlib import Path
+from buildpreprocess import generate_maximally_complex_csv
+
+# this program will generate a csv file with a predefined number of colums and rows (preferabli each value unique)
+
+if __name__ == "__main__":
+    # taking in a commandline argument to determine the number of rows
+    numb_rows = int(sys.argv[1])
+
+    generate_maximally_complex_csv(numb_rows)
+
+    # creating a temp file
+    temp_dir = Path("temp_dir")
+
+    # Run the build command using the created stress.csv in the temp_dir
+    os.chdir(temp_dir)
+    os.system("csvcubed build stress.csv -c ../test-qube-config.json")
