b5e2c8cf434870a9e9d78e21d91db3c99d61c253
NickPapONS <nick.papoutsakis@ons.gov.uk>
2022-12-07 16:22:06 +0000

    Unit test for error status code in response

--- tests/unit/utils/test_json.py
+++ tests/unit/utils/test_json.py
@@ -72,6 +72,37 @@ def test_load_local_when_http_request_fails():
             expected = json.load(f)
             assert json_document == expected
 
+# This fixture is a WIP way of possibly using the same fixture for multiple tests instead of duplicating them and changing the input url.
+# May not keep it, may improve it further.
+@pytest.fixture(autouse=True)
+def err_code_dummy_map_url():
+    def add_dummy_url(input_url):
+        map_url_to_file_path[input_url] = (
+        APP_ROOT_DIR_PATH / "schema" / "cube-config" / "v1_3" / "schema.json"
+        )
+        yield None
+        del map_url_to_file_path[input_url]
+    return add_dummy_url
+
+def test_load_local_when_bad_status_code(err_code_dummy_map_url):
+    """
+    Ensures that a local copy of a document is returned when a HTTP request returns a response
+     with a 4**/5** status code.
+    """
+    input_url = "https://purl.org/csv-cubed/qube-config/v1.3X"
+    err_code_dummy_map_url("https://purl.org/csv-cubed/qube-config/v1.3X")
+    with session.cache_disabled():
+        json_document = load_json_document(
+            input_url
+        )
+
+        expected_document = (
+            APP_ROOT_DIR_PATH / "schema" / "cube-config" / "v1_3" / "schema.json"
+        )
+        with open(expected_document, "r") as f:
+            expected = json.load(f)
+            assert json_document == expected
+
 
 if __name__ == "__main__":
     pytest.main()
