93f39356a5ad0bc7f8779a8f5942475318114438
CharlesRendle <charles.rendle@ons.gov.uk>
2022-12-20 16:11:40 +0000

    Addressing PR comments

--- src/csvcubed/utils/cache.py
+++ src/csvcubed/utils/cache.py
@@ -8,12 +8,12 @@ from requests.adapters import BaseAdapter, HTTPAdapter
 from requests_cache import CachedSession
 from csvcubed.utils.createlocalcopyresponse import _create_local_copy_response
 
-
+from csvcubed.utils.log import debug_log_exception
 from csvcubed.definitions import APP_ROOT_DIR_PATH
 
 _logger = logging.getLogger(__name__)
 
-_hard_codes_map_url_to_file_path = {
+_hard_coded_map_url_to_file_path = {
     "//purl.org/csv-cubed/qube-config/v1.0": APP_ROOT_DIR_PATH
     / "schema"
     / "cube-config"
@@ -64,8 +64,9 @@ _hard_codes_map_url_to_file_path = {
 
 def _get_url_to_file_path_map() -> Dict[str, Path]:
     """
-    Creates a dictionary that maps URLs requested by load_json_document to their corresponding local storage files,
-    so they can be retrieved in case the HTTP request fails during csvcubed build process.
+    Dynamically adds template URLs + corresponding files to the hardcoded map:
+    _hard_coded_map_url_to_file_path for use in schema mocking and when a local
+    copy of a file must be retrieved when an HTTP(S) request fails.
     """
 
     templates_dir = APP_ROOT_DIR_PATH / "readers" / "cubeconfig" / "v1_0" / "templates"
@@ -75,7 +76,7 @@ def _get_url_to_file_path_map() -> Dict[str, Path]:
     if not any(template_files):
         raise ValueError(f"Couldn't find template files in {templates_dir}.")
 
-    map_uri_to_file_path = copy.deepcopy(_hard_codes_map_url_to_file_path)
+    map_uri_to_file_path = copy.deepcopy(_hard_coded_map_url_to_file_path)
 
     for template_file in template_files:
         relative_file_path = str(template_file.relative_to(templates_dir))
@@ -91,7 +92,12 @@ def _get_url_to_file_path_map() -> Dict[str, Path]:
 map_url_to_file_path = _get_url_to_file_path_map()
 
 
-class CustomAdapterServeSomeFilesLocally(BaseAdapter):
+class AdapterToServeLocalFileWhenHTTPRequestFails(BaseAdapter):
+    """
+    A custom transport adapter which returns a local copy of the requested file only
+    in the event that an HTTP(S) request fails likely due to connectivity issues.
+    """
+
     http_adapter: HTTPAdapter
 
     def __init__(self):
@@ -116,31 +122,28 @@ class CustomAdapterServeSomeFilesLocally(BaseAdapter):
             )
 
         except requests.exceptions.ConnectionError as e:
-            try:
-                _logger.debug("The connection error has been found")
+            debug_log_exception(_logger, e)
+            _logger.error("A connection error has been found")
 
-                path_to_local_file = _generate_path_to_local_file(request.url)
-
-                _logger.debug("The local file path is: %s", path_to_local_file)
-
-                return _create_local_copy_response(path_to_local_file, request)
-
-            except KeyError:
+            path_to_local_file = _generate_path_to_local_file(request.url)
+            if path_to_local_file is None:
                 raise FileNotFoundError(
                     f"URL {request.url} did not produce a response and a local copy could not be found at the corresponding mapped path."
                 ) from e
 
+            _logger.error("The local file path is: %s", path_to_local_file)
+
+            return _create_local_copy_response(path_to_local_file, request)
+
         if response.status_code >= 400 and response.status_code < 600:
             _logger.info(f"The status code is {response.status_code}")
-            try:
-                path_to_local_file = _generate_path_to_local_file(request.url)
-                return _create_local_copy_response(path_to_local_file, request, response)
-
-            except KeyError:
+            path_to_local_file = _generate_path_to_local_file(request.url)
+            if path_to_local_file is None:
                 raise FileNotFoundError(
                     f"URL {request.url} produced a invalid response and a local copy could not be found at the corresponding mapped path."
                 )
 
+            return _create_local_copy_response(path_to_local_file, request, response)
 
         return response
 
@@ -148,21 +151,15 @@ class CustomAdapterServeSomeFilesLocally(BaseAdapter):
         self.http_adapter.close()
 
 
-def _generate_path_to_local_file(request_url: str) -> Path:
-    trimmed_url = str(request_url).removeprefix("https:")
-    if request_url[len(request_url) - 1] == "/":
-        path_to_local_file = map_url_to_file_path.get(
-            trimmed_url[: len(trimmed_url) - 1]
-        )
-    else:
-        path_to_local_file = map_url_to_file_path.get(trimmed_url)
-
-    if path_to_local_file is None:
-        raise KeyError
+def _generate_path_to_local_file(request_url: str) -> Optional[Path]:
+    trimmed_url = (
+        str(request_url).removeprefix("https:").removeprefix("http:").removesuffix("/")
+    )
+    path_to_local_file = map_url_to_file_path.get(trimmed_url)
 
     return path_to_local_file
 
 
 session = CachedSession(cache_control=True, use_cache_dir=True)
-session.mount("http://", CustomAdapterServeSomeFilesLocally())
-session.mount("https://", CustomAdapterServeSomeFilesLocally())
+session.mount("http://", AdapterToServeLocalFileWhenHTTPRequestFails())
+session.mount("https://", AdapterToServeLocalFileWhenHTTPRequestFails())
