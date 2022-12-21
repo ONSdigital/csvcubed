ff3406bb39260fd47b1eba41b39348feacfd9a4e
CharlesRendle <charles.rendle@ons.gov.uk>
2022-12-21 10:09:56 +0000

    Removes if request.url is None logic

--- src/csvcubed/utils/cache.py
+++ src/csvcubed/utils/cache.py
@@ -109,8 +109,6 @@ class AdapterToServeLocalFileWhenHTTPRequestFails(BaseAdapter):
         _logger.debug(
             "This is the HTTP(s) adapter sending the request: %s", request.url
         )
-        if request.url is None:
-            raise requests.exceptions.URLRequired
         try:
             response = self.http_adapter.send(
                 request,
