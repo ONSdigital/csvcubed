b1a3257f58ab6749b3634cce0a74230413957c0a
Rob Barry <rob.barry@ons.gov.uk>
2023-01-31 14:47:22 +0000

    Hail Mary

--- windows-testing-setup.ps1
+++ windows-testing-setup.ps1
@@ -53,7 +53,7 @@ $path += ";$pwd"
 Write-Output "=== Installing sparql-test-runner ==="
 
 # &"$curlExe" -s 'https://github.com/GSS-Cogs/sparql-test-runner/releases/download/v0.0.1/sparql-test-runner-1.4.zip' -o 'sparql-test-runner.zip'
-Invoke-WebRequest -Authentication None -Uri "https://github.com/GSS-Cogs/sparql-test-runner/releases/download/v0.0.1/sparql-test-runner-1.4.zip" -OutFile "sparql-test-runner.zip"
+Invoke-WebRequest -SkipHttpErrorCheck -MaximumRetryCount 10 -RetryIntervalSec 1 -SkipCertificateCheck -AllowUnencryptedAuthentication -SkipHeaderValidation -Uri "https://github.com/GSS-Cogs/sparql-test-runner/releases/download/v0.0.1/sparql-test-runner-1.4.zip" -OutFile "sparql-test-runner.zip"
 Expand-Archive -LiteralPath sparql-test-runner.zip -DestinationPath .
 
 $sparqlTestRunnerBinDir = (Get-Item sparql-test-runner-1.4/bin | Resolve-Path).Path.Substring(38)
