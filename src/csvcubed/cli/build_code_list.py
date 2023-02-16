42eebcfc9b5179708d258fb3955ff6225bb4d52c
Jozsef K <43893201+nimshi89@users.noreply.github.com>
2023-02-16 14:23:42 +0000

    f

--- src/csvcubed/cli/build_code_list.py
+++ src/csvcubed/cli/build_code_list.py
@@ -8,7 +8,6 @@ import logging
 from pathlib import Path
 from typing import List, Optional, Tuple
 
-from csvcubed.models.codelistconfig.code_list_config import CodeListConfig
 from csvcubed.models.cube.qb.components import NewQbCodeList
 from csvcubed.models.jsonvalidationerrors import JsonSchemaValidationError
 from csvcubed.models.validationerror import ValidationError
@@ -18,42 +17,11 @@ from csvcubed.readers.codelistconfig.codelist_schema_versions import (
 )
 from csvcubed.readers.cubeconfig.utils import load_resource
 from csvcubed.utils.cli import log_validation_and_json_schema_errors
-from csvcubed.utils.validators.schema import (
-    map_to_internal_validation_errors,
-    validate_dict_against_schema,
-)
 from csvcubed.writers.skoscodelistwriter import SkosCodeListWriter
 
 _logger = logging.getLogger(__name__)
 
 # csvcubed code-list build <some-config-file.json>
-
-
-def _extract_and_validate_code_list(
-    code_list_config_path: Path,
-) -> Tuple[NewQbCodeList, List[JsonSchemaValidationError], List[ValidationError]]:
-    """Fill this in"""
-    code_list_config, code_list_config_dict = CodeListConfig.from_json_file(
-        code_list_config_path
-    )
-    schema = load_resource(code_list_config.schema)
-
-    unmapped_schema_validation_errors = validate_dict_against_schema(
-        value=code_list_config_dict, schema=schema
-    )
-
-    code_list_schema_validation_errors = map_to_internal_validation_errors(
-        schema, unmapped_schema_validation_errors
-    )
-
-    code_list = NewQbCodeList(
-        code_list_config.metadata, code_list_config.new_qb_concepts
-    )
-    return (
-        code_list,
-        code_list_schema_validation_errors,
-        code_list.pydantic_validation(),
-    )
 
 
 def build_code_list(
@@ -62,17 +30,14 @@ def build_code_list(
     fail_when_validation_error_occurs: bool = False,
     validation_errors_file_name: Optional[str] = None,
 ):
-    # (
-    #     code_list,
-    #     json_schema_validation_errors,
-    #     validation_errors,
-    # ) = _extract_and_validate_code_list(config_path)
 
     code_list_deserialiser = get_code_list_versioned_deserialiser(config_path)
 
-    code_list, json_schema_validation_errors, validation_errors = sort_out(
-        code_list_deserialiser, config_path
-    )
+    (
+        code_list,
+        json_schema_validation_errors,
+        validation_errors,
+    ) = code_list_deserialiser(config_path)
 
     log_validation_and_json_schema_errors(
         output_directory,
@@ -104,14 +69,3 @@ def get_code_list_versioned_deserialiser(
         return get_deserialiser_for_code_list_schema(config.get("$schema"))
     else:
         return get_deserialiser_for_code_list_schema(None)
-
-
-def sort_out(
-    the_data_we_need_now: CodeListConfigDeserialiser, config_path: Path
-) -> Tuple[NewQbCodeList, List[JsonSchemaValidationError], List[ValidationError]]:
-
-    code_list, json_schema_validation_errors, validation_errors = the_data_we_need_now(
-        config_path
-    )
-
-    return code_list, json_schema_validation_errors, validation_errors
--- src/csvcubed/readers/codelistconfig/codelist_schema_versions.py
+++ src/csvcubed/readers/codelistconfig/codelist_schema_versions.py
@@ -10,10 +10,16 @@ from enum import Enum
 from pathlib import Path
 from typing import Callable, List, Optional, Tuple
 
+from csvcubed.models.codelistconfig.code_list_config import CodeListConfig
 from csvcubed.models.cube.qb.components.codelist import NewQbCodeList
 from csvcubed.models.jsonvalidationerrors import JsonSchemaValidationError
 from csvcubed.models.validationerror import ValidationError
+from csvcubed.readers.cubeconfig.utils import load_resource
 from csvcubed.readers.cubeconfig.v1 import configdeserialiser as v1_configdeserialiser
+from csvcubed.utils.validators.schema import (
+    map_to_internal_validation_errors,
+    validate_dict_against_schema,
+)
 
 _logger = logging.getLogger(__name__)
 
@@ -61,6 +67,33 @@ class CodeListConfigJsonSchemaMinorVersion(Enum):
     v1 = 1
 
 
+def _extract_and_validate_code_list_v1(
+    code_list_config_path: Path,
+) -> Tuple[NewQbCodeList, List[JsonSchemaValidationError], List[ValidationError]]:
+    """Fill this in"""
+    code_list_config, code_list_config_dict = CodeListConfig.from_json_file(
+        code_list_config_path
+    )
+    schema = load_resource(code_list_config.schema)
+
+    unmapped_schema_validation_errors = validate_dict_against_schema(
+        value=code_list_config_dict, schema=schema
+    )
+
+    code_list_schema_validation_errors = map_to_internal_validation_errors(
+        schema, unmapped_schema_validation_errors
+    )
+
+    code_list = NewQbCodeList(
+        code_list_config.metadata, code_list_config.new_qb_concepts
+    )
+    return (
+        code_list,
+        code_list_schema_validation_errors,
+        code_list.pydantic_validation(),
+    )
+
+
 def get_deserialiser_for_code_list_schema(
     maybe_schema_path: Optional[str],
 ) -> CodeListConfigDeserialiser:
@@ -76,13 +109,13 @@ def get_deserialiser_for_code_list_schema(
         schema_path
     )
     _logger.info(
-        f"Using schema version {schema_version_major.value}.{schema_version_minor.value}"
+        "Using schema version %s %s",
+        schema_version_major.value,
+        schema_version_minor.value,
     )
 
     if schema_version_major == CodeListConfigJsonSchemaMajorVersion.v1:
-        return v1_configdeserialiser.get_deserialiser(
-            schema_path, schema_version_minor.value
-        )
+        return _extract_and_validate_code_list_v1
     else:
         raise ValueError(f"Unhandled major schema version {schema_version_major}")
 
