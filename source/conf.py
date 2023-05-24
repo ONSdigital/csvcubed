99935c40275e1f7dd7dbb4374f799d687acb1fe5
Jozsef K <43893201+nimshi89@users.noreply.github.com>
2023-05-24 10:34:39 +0000

    g

--- source/conf.py
+++ source/conf.py
@@ -17,38 +17,38 @@ release = "0.3.5"
 extensions = [
     "sphinx.ext.autodoc",
 ]
-include_patterns = [
-    "index.rst",
-    "modules.rst",
-    "csvcubed*.rst",
-]
+# include_patterns = [
+#     "index.rst",
+#     "modules.rst",
+#     "csvcubed*.rst",
+# ]
 templates_path = ["_templates"]
-exclude_patterns = [
-    "csvcubed.utils.skos*.rst",
-    "csvcubed.cli.build*.rst",
-    "csvcubed.cli.pull*.rst",
-    "csvcubed.cli.error_mapping*.rst",
-    "csvcubed.cli.entry*.rst",
-    "csvcubed.utils.date*.rst",
-    "csvcubed.utils.dict*.rst",
-    "csvcubed.utils.file*.rst",
-    "csvcubed.utils.iterables*.rst",
-    "csvcubed.utils.json*.rst",
-    "csvcubed.utils.log*.rst",
-    "csvcubed.utils.p*.rst",
-    "csvcubed.utils.rdf*.rst",
-    "csvcubed.utils.u*.rst",
-    "csvcubed.utils.v*.rst",
-    "csvcubed.utils.t*.rst",
-    "csvcubed.cli.codelist*.rst",
-    "csvcubed.utils.qb*.rst",
-    "csvcubed.utils.c*.rst",
-    "csvcubed.models*.rst",
-    "csvcubed.readers*.rst",
-    "csvcubed.writers*.rst",
-    "csvcubed.definitions*.rst",
-    "csvcubed.inputs*.rst",
-]
+# exclude_patterns = [
+#     "csvcubed.utils.skos*.rst",
+#     "csvcubed.cli.build*.rst",
+#     "csvcubed.cli.pull*.rst",
+#     "csvcubed.cli.error_mapping*.rst",
+#     "csvcubed.cli.entry*.rst",
+#     "csvcubed.utils.date*.rst",
+#     "csvcubed.utils.dict*.rst",
+#     "csvcubed.utils.file*.rst",
+#     "csvcubed.utils.iterables*.rst",
+#     "csvcubed.utils.json*.rst",
+#     "csvcubed.utils.log*.rst",
+#     "csvcubed.utils.p*.rst",
+#     "csvcubed.utils.rdf*.rst",
+#     "csvcubed.utils.u*.rst",
+#     "csvcubed.utils.v*.rst",
+#     "csvcubed.utils.t*.rst",
+#     "csvcubed.cli.codelist*.rst",
+#     "csvcubed.utils.qb*.rst",
+#     "csvcubed.utils.c*.rst",
+#     "csvcubed.models*.rst",
+#     "csvcubed.readers*.rst",
+#     "csvcubed.writers*.rst",
+#     "csvcubed.definitions*.rst",
+#     "csvcubed.inputs*.rst",
+# ]
 
 
 # -- Options for HTML output -------------------------------------------------
