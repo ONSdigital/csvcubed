78d5eae583cd94d27b47dab4e57c88adceb22e2f
NickPapONS <nick.papoutsakis@ons.gov.uk>
2023-08-08 14:00:29 +0000

    making cwvws invalid to cause csvwcheck error

--- src/csvcubed/writers/skoscodelistwriter.py
+++ src/csvcubed/writers/skoscodelistwriter.py
@@ -175,7 +175,7 @@ class SkosCodeListWriter(WriterBase):
             }
         )
         return {
-            "@context": "http://www.w3.org/ns/csvw",
+            # "@context": "http://www.w3.org/ns/csvw",
             "columns": csvw_columns,
             "aboutUrl": self.uri_helper.get_concept_uri("{+uri_identifier}"),
             "primaryKey": "uri_identifier",
