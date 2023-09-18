91460446bcf2ff6f156de3b489b4ce1858274d2e
NickPapONS <nick.papoutsakis@ons.gov.uk>
2023-09-18 15:40:59 +0000

    For windows test results

--- src/csvcubed/models/rdf/qbdatasetincatalog.py
+++ src/csvcubed/models/rdf/qbdatasetincatalog.py
@@ -7,7 +7,7 @@ from csvcubedmodels.rdf import dcat, qb
 from csvcubed.models.rdf import prov
 
 
-class QbDataSetInCatalog(qb.DataSet, dcat.Distribution, prov.Entity):
+class QbDataSetInCatalog(qb.DataSet, dcat.Dataset, prov.Entity):
     """
     Represents both a qb:DataSet and a dcat:Dataset in one node. Means that we don't have to link
     between the two.
@@ -15,4 +15,4 @@ class QbDataSetInCatalog(qb.DataSet, dcat.Distribution, prov.Entity):
 
     def __init__(self, uri: str):
         qb.DataSet.__init__(self, uri)
-        dcat.Distribution.__init__(self, uri)
+        dcat.Dataset.__init__(self, uri)
